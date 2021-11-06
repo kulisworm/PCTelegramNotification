5f2e74cc0c2ab86526444667a37bb2d77dd60fef
Kuuli <59212398+kulisworm@users.noreply.github.com>
2021-11-06 23:40:40 +0300

    Update win-install.bat

--- win-install.bat
+++ win-install.bat
@@ -2,4 +2,4 @@ pip3 install pyTelegramBotAPI
 pip3 install testresources
 pip3 install pyspectator
 pip3 install bs4
-msg "%username%" Все необходимые модули успешно установлены и готовы к работе. Убедитесь , что вы установили python3 с Microsoft store!!!
+msg "%username%" Done!:)
